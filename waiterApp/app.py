e36f99aeed39327dad57b4367528cb1de64b84f8
wallaceleonel <wallace.leonell@outlook.com>
2022-07-27 17:24:24 -0300

    correção de logica produto e nome

--- waiterApp/app.py
+++ waiterApp/app.py
@@ -9,27 +9,30 @@ from pydantic import BaseModel, Field
 from tinydb import TinyDB, Query
 from tinydb.storages import MemoryStorage
 
+
 server = Flask(__name__)
-spec = FlaskPydanticSpec('flask', title='Waiter Api')
+spec = FlaskPydanticSpec('flask', title='Waiter Api ')
 spec.register(server)
 database = TinyDB(storage=MemoryStorage)
 c = count()
 
+
 class QueryProduto(BaseModel):
     id: Optional[int]
-    Produto: Optional[str]
+    nome: Optional[str]
     valor: Optional[int]
 
 
 class Produto(BaseModel):
     id: Optional[int] = Field(default_factory=lambda: next(c))
-    Produto: str
+    nome: str
     valor: int
 
 
 class Produtos(BaseModel):
-    Produto: list[Produto]
+    produto: list[Produto]
     count: int
+
 
 @server.get('/produtos')  # Rota, endpoint, recurso ...
 @spec.validate(
@@ -37,7 +40,7 @@ class Produtos(BaseModel):
     resp=Response(HTTP_200=Produtos)
 )
 def buscar_produtos():
-    '''Busca produtos no banco de dados'''  
+    """Retorna todos os Produtos da base de dados."""
     query = request.context.query.dict(exclude_none=True)
     todos_os_produtos = database.search(
         Query().fragment(query)
@@ -52,40 +55,43 @@ def buscar_produtos():
 @server.get('/produtos/<int:id>')
 @spec.validate(resp=Response(HTTP_200=Produto))
 def buscar_produto(id):
-    '''Retorna todas as produtos na base de dados.'''
+    """Retorna todos os Produtos da base de dados."""
     try:
-        pessoa = database.search(Query().id == id)[0]
+        produto = database.search(Query().id == id)[0]
     except IndexError:
-        return{'message': 'Produto not found!'}, 404
-    return jsonify(pessoa)
+        return {'message': 'Produto not found!'}, 404
+    return jsonify(produto)
+
 
 @server.post('/produtos')
 @spec.validate(
-    body=Request(Produto),resp=Response(HTTP_200=Produto))
-
+    body=Request(Produto), resp=Response(HTTP_201=Produto)
+)
 def inserir_produto():
-    '''insere um produto no banco de dados'''
+    """Insere um Produto no banco de dados."""
     body = request.context.body.dict()
     database.insert(body)
     return body
+
 
 @server.put('/produtos/<int:id>')
 @spec.validate(
     body=Request(Produto), resp=Response(HTTP_201=Produto)
 )
 def altera_produto(id):
-    '''Altera caracteristicas do produto'''
+    """Altera um Produto no banco de dados."""
     Produto = Query()
     body = request.context.body.dict()
     database.update(body, Produto.id == id)
     return jsonify(body)
 
-@server.delete('/produto/<int:id>')
+
+@server.delete('/produtos/<int:id>')
 @spec.validate(resp=Response('HTTP_204'))
-def deletar_produto(id):
-    '''Remove produto do banco de dados'''
+def deleta_produto(id):
+    """Remove um Produto do banco de dados."""
     database.remove(Query().id == id)
     return jsonify({})
 
-server.run()
 
+server.run()
